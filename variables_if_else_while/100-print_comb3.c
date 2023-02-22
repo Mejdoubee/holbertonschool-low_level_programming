2e72001d0ee26a1a74c59105a346b1afc42e8f30
Mejdoubee <recovery.mejdoub@gmail.com>
2023-02-22 21:50:36 +0100

    print comb

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,20 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 48; a <= 57; a++)
+	for (a = 48; a <= 56; a++)
+	{
 		for (b = 48; b <= 57; b++)
-		{
-			putchar(a);
-			putchar(b);
-			if ((a != '9') || (b != '9'))
+			if (a < b)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(a);
+				putchar(b);
+				if ((a != 56) || (b != 57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+	}
 			}
-		}
+	putchar('\n');
 	return (0);
 }
