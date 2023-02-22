9de060d3e5346af225d1ccca2e3bea60ef7a5363
Mejdoubee <recovery.mejdoub@gmail.com>
2023-02-22 16:55:13 +0100

    123456789

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+**/
+int main(void)
+{
+	int n;
+
+	for (n = '1'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
