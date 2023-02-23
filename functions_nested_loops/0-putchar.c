94a0cac7a3cc69a20035d36e1cd11c38f541b1bf
Mejdoubee <recovery.mejdoub@gmail.com>
2023-02-23 16:54:20 +0100

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 ( Success)
+*/
+
+int main(void)
+{
+	int c;
+	char b[] = "_putchar";
+
+	for (c = 0 ; c <= 7; c++)
+		{
+		_putchar(b[c]);
+		}
+	_putchar('\n');
+	return (0);
+}
