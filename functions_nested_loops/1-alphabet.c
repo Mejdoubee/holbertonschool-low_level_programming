841bbc096c0e08695b769e35b47bc78ae8882f29
Mejdoubee <recovery.mejdoub@gmail.com>
2023-02-23 17:40:52 +0100

    a..z

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	int c;
+	for (c = 97; c <= 122; c++)
+		{
+		_putchar(c);
+		}
+	_putchar('\n');
+}
