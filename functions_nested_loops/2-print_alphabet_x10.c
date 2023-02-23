f37b372ca5777b5563c1f79c33a2b8c5a1feeee5
Mejdoubee <recovery.mejdoub@gmail.com>
2023-02-23 18:05:51 +0100

    a..z *10 fix new line

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,9 @@ void print_alphabet_x10(void)
 	int a, b;
 
 	for (a = 48 ; a <= 57; a++)
+		{
 		for (b = 97; b <= 122 ; b++)
-		_putchar(b);
-	_putchar('\n');
+			_putchar(b);
+		_putchar('\n');
+		}
 }
