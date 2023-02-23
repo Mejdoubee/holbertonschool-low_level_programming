1d518a9865bc795081af19bb4b62cb97423a727a
Mejdoubee <recovery.mejdoub@gmail.com>
2023-02-23 18:01:34 +0100

    a..z *10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Entry point
+*
+* Return: Defaut value
+*/
+
+void print_alphabet_x10(void)
+{
+	int a, b;
+
+	for (a = 48 ; a <= 57; a++)
+		for (b = 97; b <= 122 ; b++)
+		_putchar(b);
+	_putchar('\n');
+}
