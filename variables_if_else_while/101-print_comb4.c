ec1f721c9a353cb06eb3da32ca98d0a333f97093
Mejdoubee <recovery.mejdoub@gmail.com>
2023-02-26 04:34:52 +0100

    adv 101 print comb4

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i, l, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (l = i + 1; l <= 8; l++)
+		{
+			for (k = l + 1; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(l + '0');
+				putchar(k + '0');
+				if (i != 7 || l != 8 || k != 9)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
