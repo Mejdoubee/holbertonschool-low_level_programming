6323256f6b816633febd807a2e160a598bd3300c
Mejdoubee <recovery.mejdoub@gmail.com>
2023-02-27 18:23:24 +0100

    diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,10 +8,26 @@
 
 void print_diagonal(int n)
 {
+/**
 	int a;
 
+	if (n > 0)
 	for (a = 0; a < n; a++)
+	{
 		_putchar(' ');
 	_putchar('\\');
+	_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+*/
+	int a;
+
+	for(a = 0; a < n; a++)
+		_putchar(' ');
+	if(a > 0)
+		_putchar('\\');
 	_putchar('\n');
 }
