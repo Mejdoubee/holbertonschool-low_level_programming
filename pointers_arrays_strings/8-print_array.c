91ce38fac4f564c2172a524576fc1d4dc4d10672
Mejdoubee <recovery.mejdoub@gmail.com>
2023-03-05 11:47:27 +0100

    element of an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - print number of elem of array
+* @a: array
+* @n: int
+* Return: nb of element
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	if (i < n - 1)
+		printf("%i, ", a[i]);
+	else
+		printf("%i\n", a[i]);
+	}
+}
