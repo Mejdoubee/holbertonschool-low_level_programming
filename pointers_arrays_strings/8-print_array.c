407ecebaf7678e1f8c55a19d36515044568f347d
Mejdoubee <recovery.mejdoub@gmail.com>
2023-03-05 12:11:20 +0100

    element of an array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -15,9 +15,9 @@ void print_array(int *a, int n)
 		return;
 	for (i = 0; i < n; i++)
 	{
-	if (i != n - 1)
-		printf("%i, ", a[i]);
-	else
-		printf("%i\n", a[i]);
+		printf("%i", a[i]);
+		if (i != n - 1)
+			printf(", ");
 	}
+	printf("\n");
 }
