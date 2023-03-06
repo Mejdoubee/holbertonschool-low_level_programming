767f6c2d2ab9dad7f64df1024c7eb32ac5e33ab8
Mejdoubee <recovery.mejdoub@gmail.com>
2023-03-06 14:47:42 +0100

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,10 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	if (n > i)
+	while (n > i)
+	{
 		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
