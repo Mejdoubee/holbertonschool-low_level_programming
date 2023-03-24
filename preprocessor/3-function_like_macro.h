d25b3628a1ff5f73934238364217eb7141b8b09a
Mejdoubee <recovery.mejdoub@gmail.com>
2023-03-24 15:14:29 +0100

    abs(x)

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _ABS_H
-#define _ABS_H
-#define ABS(x) ((x < 0) ? (-x) : (x))
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 #endif
