5936d249160b160c64c177bb6b878f16c9984567
Mejdoubee <recovery.mejdoub@gmail.com>
2023-03-25 16:47:24 +0100

    print struct

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ void print_dog(struct dog *d)
 	printf("name: ");
 	d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
 	printf("age: ");
-	d->age <= 0 ? printf("(nil)\n") : printf("%f\n", d->age);
+	d->age <= 0 ? printf("%f\n", d->age) : printf("%f\n", d->age);
 	printf("owner: ");
 	d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
 	}
