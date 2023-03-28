e4793d8930bc15bf8320abb5bd6aa9e8e18b98f8
Mejdoubee <recovery.mejdoub@gmail.com>
2023-03-28 21:35:47 +0100

    print anyth_ng :3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,92 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+* print_char - print char
+* @args: of type char
+*
+*/
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+		return;
+}
+
+/**
+* print_integer - print integer
+* @args: of int type
+*
+*/
+void print_integer(va_list args)
+{
+	printf("%d", va_arg(args, int));
+		return;
+}
+
+/**
+* print_float - print float
+* @args: of type float
+*
+*/
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+* print_string - print string
+* @args: string type
+*
+*/
+void print_string(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char *);
+	printf("%s", !s ? "nil" : s);
+}
+
+/**
+* print_all - function that prints anything
+* @format: list of types of arguments passed to the function
+* Return: no return
+*
+*/
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int start = 0, i = 0, j;
+	char *sep = ", ";
+
+	print_f formats[] = {
+			{'c', print_char},
+			{'i', print_integer},
+			{'f', print_float},
+			{'s', print_string},
+			{0, NULL}
+	};
+
+	va_start(args, format);
+
+	while (format[i])
+	{
+		j = 0;
+		while (formats[j].type)
+		{
+			if (format[i] == formats[j].type)
+			{
+				if (start)
+				{
+					printf("%s", sep);
+				start = 1;
+				formats[j].f(args);
+				break;
+				}
+			}
+			j++;
+		}
+	i++;
+	}
+	va_end(args);
+	printf("\n");
+}
